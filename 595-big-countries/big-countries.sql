8ea59cf2919d1a98e246c770ef560b1b60dd5682
Dishant Barot <161105266+dishantbarot@users.noreply.github.com>
2026-02-12 15:06:41 +0530

    Time: 291 ms (45.41%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -1,4 +1,3 @@
 # Write your MySQL query statement below
-SELECT name, area, population FROM World WHERE area >= 3000000
-UNION
-SELECT name, area, population FROM World WHERE population >= 25000000;
+SELECT name,area,population FROM World
+WHERE area >= 3000000 OR population >= 25000000;
